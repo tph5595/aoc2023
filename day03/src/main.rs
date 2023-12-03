deb01f855818a1826abc5b45bc863b05f22810d6
Taylor Henderson <tph5595@verizon.net>
2023-12-03 10:42:34 -0500

    test working but now full??

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -10,20 +11,9 @@ struct Number{
     end: (usize, usize),
 }
 
-#[derive(Debug, PartialEq)]
-struct Symbol {
-    loc: (usize, usize),
-}
-
-#[derive(Debug, PartialEq)]
-enum Data {
-    N(Number),
-    S(Symbol)
-    
-}
-
-fn parse(lines: Vec<String>) -> Vec<Data>{
-    let mut values = Vec::new();
+fn parse(lines: Vec<String>) -> (Vec<Number>, HashSet<(usize,usize)>){
+    let mut n = Vec::new();
+    let mut s = HashSet::new();
     let mut current = 0;
     let mut start = None;
     for (row, l) in lines.iter().enumerate(){
@@ -37,21 +27,21 @@ fn parse(lines: Vec<String>) -> Vec<Data>{
                 },
                 '.' => {
                     if start != None && col != 0{
-                        values.push(Data::N(Number { value: current as usize, 
+                        n.push(Number { value: current as usize, 
                             start: start.unwrap(), 
                             end: (col-1, row)
-                        }))
+                        })
                     }
                     current = 0;
                     start = None;
                 },
                 _ => {
-                    values.push(Data::S(Symbol { loc: (col,row) }));
+                    s.insert((col,row));
                     if start != None && col != 0{
-                        values.push(Data::N(Number { value: current as usize, 
+                        n.push(Number { value: current as usize, 
                             start: start.unwrap(), 
                             end: (col-1, row)
-                        }))
+                        })
                     }
                     current = 0;
                     start = None;
@@ -59,17 +49,44 @@ fn parse(lines: Vec<String>) -> Vec<Data>{
             };
         }
     }
-    values
+    (n,s)
+}
+
+fn gen_set(n: &Number) -> HashSet<(usize, usize)>{
+    let mut n_set = HashSet::new();
+    
+    for r in n.start.1.saturating_sub(1)..=n.end.1.saturating_add(1){
+        for c in n.start.0.saturating_sub(1)..=n.end.0.saturating_add(1){
+            n_set.insert((c,r));
+        }
+    }
+    n_set
+}
+
+fn find_good(numbers: Vec<Number>, symbols: HashSet<(usize, usize)>)-> Vec<Number>{
+    let mut valid = Vec::new();
+    for n in numbers{
+        let n_set = gen_set(&n);
+        if !symbols.is_disjoint(&n_set){
+            valid.push(n);
+        }
+    }
+    valid
 }
 
 fn p1 (){
-    if let Ok(lines) = read_lines("./test.txt") {
+    if let Ok(lines) = read_lines("./input.txt") {
         let raw_data: Vec<String>= lines
             .into_iter()
             .filter_map(|item| item.ok())
             .collect();
-        let data = parse(raw_data);
-        println!("{:?}", data);
+        let (numbers, symbols) = parse(raw_data);
+        let good_numbers = find_good(numbers, symbols);
+
+        let s: usize = good_numbers.iter().map(|i| i.value).sum();
+        
+        // println!("{:?}", good_numbers);
+        println!("{:?}", s);
     }
     else {
         println!("File not found")
