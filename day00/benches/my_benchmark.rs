94b319e3fc389feeddbfee90840e1f0fb4bd32f6
Taylor Henderson <tph5595@verizon.net>
2023-12-06 10:43:10 -0500

    bench typo

--- day00/benches/my_benchmark.rs
+++ day00/benches/my_benchmark.rs
@@ -3,7 +3,7 @@ use day00::{p1, p2};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("p1", |b| b.iter(|| p1("./src/input.txt")));
-    c.bench_function("p2 20", |b| b.iter(|| p2("./src/input.txt")));
+    c.bench_function("p2", |b| b.iter(|| p2("./src/input.txt")));
 }
 
 criterion_group!(benches, criterion_benchmark);
