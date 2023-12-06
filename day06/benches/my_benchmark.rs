07fe30022b5959cea0641d3d224d58bcf2ff19ec
Taylor Henderson <tph5595@verizon.net>
2023-12-06 10:42:44 -0500

    bench

--- day06/benches/my_benchmark.rs
+++ day06/benches/my_benchmark.rs
@@ -1,9 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use day00::{p1, p2};
+use day06::{p1, p2};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("p1", |b| b.iter(|| p1("./src/input.txt")));
-    c.bench_function("p2 20", |b| b.iter(|| p2("./src/input.txt")));
+    c.bench_function("p2", |b| b.iter(|| p2("./src/input.txt")));
 }
 
 criterion_group!(benches, criterion_benchmark);
