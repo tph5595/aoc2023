1ff21b7bee1ee47e0a464ac8cbdc628b9787a987
Taylor Henderson <tph5595@verizon.net>
2023-12-07 12:23:44 -0500

    p1 is p2

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -3,29 +3,39 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::str::FromStr;
-use std::u32;
+use std::{u32, usize};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Hand{
     cards: Vec<u32>,
     bet: u32,
-    value: u32
+    value: u32, 
+    jokes: usize
 }
 
 impl Hand {
-    fn calc_value(cards: &Vec<u32>) -> u32{
+    fn calc_value(cards: &Vec<u32>, jokes: usize) -> u32{
         let mut map = HashMap::new();
         for c in cards{
-            let e = map.entry(c).or_insert(0u32);
-            *e += 1;
+            // Don't count jokers
+            if *c != 1{
+                let e = map.entry(c).or_insert(0u32);
+                *e += 1;
+            }
         }
         let k: Vec<&u32> = map.values().map(|v| v as &u32).collect();
+        // let are_jokes = {
+        //     if jokes > 0{
+        //         return 1;
+        //     }
+        //     0
+        // };
         //Five of a kind, where all five cards have the same label: AAAAA
-        if k.len() == 1 {
+        if k.len() == 1 || jokes == 5 {
             return 7;
         }
         // Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-        if k.iter().any(|i| **i == 4){
+        if k.iter().any(|i| **i == (4-jokes) as u32 ) || jokes == 4{
             return 6;
         }
         // Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
@@ -33,7 +43,7 @@ impl Hand {
             return 5;
         }
         // Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-        if k.iter().any(|i| **i == 3){
+        if k.iter().any(|i| **i == (3-jokes) as u32) || jokes == 3{
             return 4;
         }
         // Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
@@ -41,7 +51,7 @@ impl Hand {
             return 3;
         }
         // One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-        if k.len() == 4{
+        if k.len() == 4 {
             return 2;
         }
         // High card, where all cards' labels are distinct: 23456
@@ -74,20 +84,24 @@ impl FromStr for Hand{
     fn from_str(input: &str) -> Result<Hand, Self::Err> {
         let mut data = input.split_whitespace();
 
-        let cards = data.next().unwrap().chars().map(|c|{
+        let cards: Vec<u32>= data.next().unwrap().chars().map(|c|{
             match c {
                 'A'=> 14,
                 'K'=> 13,
                 'Q'=> 12,
-                'J'=> 11,
+                'J'=> 1,
                 'T'=> 10,
                 _ => c.to_digit(10).unwrap(),
             }
         }).collect();
         let bet: u32 = data.next().unwrap().parse::<u32>().unwrap();
-        let value = Hand::calc_value(&cards);
+        let jokes: usize = cards.iter()
+            .filter(|i| **i == 1)
+            .map(|i| *i as usize)
+            .sum();
+        let value = Hand::calc_value(&cards, jokes);
 
-        Ok(Hand{ cards, bet, value})
+        Ok(Hand{ cards, bet, value, jokes})
     }
 }
 
@@ -99,7 +113,6 @@ pub fn p1 (file: &str) -> u32{
             .map(|i| i.parse().unwrap())
             .collect();
         data.sort();
-        // data.reverse();
         let winnings = data.iter_mut()
             .enumerate()
             .map(|(v, c)| c.bet * ((v+1) as u32)).sum();
