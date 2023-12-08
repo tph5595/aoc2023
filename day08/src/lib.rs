9067df229229faafb48f799c21a747dc6148a6e4
Taylor Henderson <tph5595@verizon.net>
2023-12-08 10:48:58 -0500

    nice

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
+use num::integer::lcm;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn p1 (file: &str) -> u32{
+pub fn p1 (file: &str) -> u128{
     if let Ok(lines) = read_lines(file) {
         let data: Vec<String>= lines
             .into_iter()
@@ -48,7 +49,60 @@ pub fn p1 (file: &str) -> u32{
     }
 }
 
-pub fn p2(_file: &str) -> usize{ 0 }
+pub fn p2(file: &str) -> u128{
+    if let Ok(lines) = read_lines(file) {
+        let data: Vec<String>= lines
+            .into_iter()
+            .filter_map(|item| item.ok())
+            .map(|i| i.parse().unwrap())
+            .collect();
+        let mut iter = data.into_iter();
+        let dir = iter.next().unwrap();
+        let trans: Vec<Vec<String>>= iter.skip(1)
+            .map(|i| 
+                 i.replace(|c: char| !(c.is_uppercase() || c.is_whitespace()), "")
+                 .split_whitespace()
+                 .map(|i| i.to_owned())
+                 .collect::<Vec<String>>())
+            .collect();
+        let starts: Vec<String> = trans.clone().iter()
+            .filter(|i| i.get(0).unwrap().chars().last().unwrap() == 'A')
+            .map(|i| i.get(0).unwrap().to_owned())
+            .collect();
+        let map: HashMap<String, (String, String)>= trans.into_iter().fold(HashMap::new(), |mut m, v| {
+                                         m.insert(v.get(0).unwrap().to_owned(), 
+                                                  (v.get(1).unwrap().to_owned(), 
+                                                   v.get(2).unwrap().to_owned()));
+                                         m
+        });
+        let mut ans: Vec<u128> = Vec::new();
+        for i in starts {
+            let mut state: String = i;
+            let mut count = 0;
+            for d in dir.chars().into_iter().cycle(){
+                if state.chars().last().unwrap() == 'Z'{
+                    break;
+                }
+                if d == 'L' 
+                {
+                    let tmp = &map.get(&state).unwrap().0 ;
+                    state = tmp.to_string();
+                    count  = count + 1;
+                }
+                else {
+                    let tmp = &map.get(&state).unwrap().1 ;
+                    state = tmp.to_string();
+                    count = count + 1;
+                }
+            }
+        ans.push(count);
+        }
+        return ans.into_iter().fold(1, |current_lcm, n| lcm(current_lcm, n));
+    }
+    else {
+        panic!("File not found")
+    }
+}
 
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -3,5 +3,5 @@ use day08::{p1, p2};
 fn main() {
     let file = "./input.txt";
     println!("p1: {:?}", p1(file));
-    println!("p2: {}", p2(file));
+    println!("p2: {:?}", p2(file));
 }
