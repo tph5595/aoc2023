dcf514660d22feb1640bc0b8d58a29084cd62358
Taylor Henderson <tph5595@verizon.net>
2023-12-09 08:33:26 -0500

    EZ

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -19,6 +19,22 @@ fn next_val(mut values: Vec<i128>) -> i128{
     s
 }
 
+fn last_val(mut values: Vec<i128>) -> i128{
+    let mut s: i128 = 0;
+    loop {
+        if values.iter().all(|x| *x == 0){
+            break;
+        }
+        s += values.first().unwrap();
+        values = values
+            .windows(2)
+            .map(|w| 
+                 w.get(0).unwrap() - w.get(1).unwrap() )
+            .collect();
+    }
+    s
+}
+
 pub fn p1 (file: &str) -> i128{
     if let Ok(lines) = read_lines(file) {
         let data: Vec<Vec<i128>>= lines
@@ -38,7 +54,24 @@ pub fn p1 (file: &str) -> i128{
     }
 }
 
-pub fn p2(_file: &str) -> usize{ 0 }
+pub fn p2 (file: &str) -> i128{
+    if let Ok(lines) = read_lines(file) {
+        let data: Vec<Vec<i128>>= lines
+            .into_iter()
+            .filter_map(|item| item.ok())
+            .map(|i| {
+                i.split_whitespace().map(|i| i.parse().unwrap()).collect()
+            })
+            .collect();
+            let next: Vec<i128> = data.into_iter()
+                .map(|v| last_val(v))
+                .collect();
+        return next.into_iter().sum();
+    }
+    else {
+        panic!("File not found")
+    }
+}
 
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
