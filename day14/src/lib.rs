87d297802810d8304138f7ed92c596a06afe0433
Taylor Henderson <tph5595@verizon.net>
2023-12-14 15:48:36 -0500

    ez didn't work

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -34,11 +34,118 @@ fn tilt_north(data: &mut Vec<Vec<u8>>){
     }
 }
 
-fn byte_to_strings(data: &Vec<Vec<u8>>)-> Vec<String>{
+fn tilt_south(data: &mut Vec<Vec<u8>>){
+    let mut next_spot: Option<usize>;
+    for col in 0..data.get(0).unwrap().len(){
+        next_spot = None;
+        for row in (0..data.len()).rev(){
+            match data[row][col] {
+                b'.' => {
+                    if next_spot == None{
+                        next_spot = Some(row);
+                    }
+                },
+                b'#' => {
+                    next_spot = None;
+                },
+                b'O' => {
+                    // If None then it cannot move
+                    if next_spot != None{
+                        // erase from current spot 
+                        data[row][col] = b'.';
+                        // put in new spot 
+                        data[next_spot.unwrap()][col] = b'O';
+                        // look for next place
+                        next_spot = Some(next_spot.unwrap() - 1);
+                    }
+                },
+                _ => unreachable!()
+            }
+        }
+    }
+}
+
+fn tilt_west(data: &mut Vec<Vec<u8>>){
+    let mut next_spot: Option<usize>;
+    for row in 0..data.len(){
+        next_spot = None;
+        for col in 0..data.get(0).unwrap().len(){
+            match data[row][col] {
+                b'.' => {
+                    if next_spot == None{
+                        next_spot = Some(col);
+                    }
+                },
+                b'#' => {
+                    next_spot = None;
+                },
+                b'O' => {
+                    // If None then it cannot move
+                    if next_spot != None{
+                        // erase from current spot 
+                        data[row][col] = b'.';
+                        // put in new spot 
+                        data[row][next_spot.unwrap()] = b'O';
+                        // look for next place
+                        next_spot = Some(next_spot.unwrap() + 1);
+                    }
+                },
+                _ => unreachable!()
+            }
+        }
+    }
+}
+
+fn tilt_east(data: &mut Vec<Vec<u8>>){
+    let mut next_spot: Option<usize>;
+    for row in 0..data.len(){
+        next_spot = None;
+        for col in (0..data.get(0).unwrap().len()).rev(){
+            match data[row][col] {
+                b'.' => {
+                    if next_spot == None{
+                        next_spot = Some(col);
+                    }
+                },
+                b'#' => {
+                    next_spot = None;
+                },
+                b'O' => {
+                    // If None then it cannot move
+                    if next_spot != None{
+                        // erase from current spot 
+                        data[row][col] = b'.';
+                        // put in new spot 
+                        data[row][next_spot.unwrap()] = b'O';
+                        // look for next place
+                        next_spot = Some(next_spot.unwrap() - 1);
+                    }
+                },
+                _ => unreachable!()
+            }
+        }
+    }
+}
+
+fn byte_to_strings(data: &Vec<Vec<u8>>)-> String{
     data
         .iter()
         .map(|bytes| String::from_utf8_lossy(bytes).to_string())
-        .collect()
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+fn cycle(data: &mut Vec<Vec<u8>>, times: usize){
+    for i in 0..times{
+        if i % 10000 == 0{
+            println!("{}", i);
+        }
+        tilt_north(data);
+        tilt_west(data);
+        tilt_south(data);
+        tilt_east(data);
+    }
+
 }
 
 pub fn p1 (file: &str) -> usize{
@@ -48,9 +155,9 @@ pub fn p1 (file: &str) -> usize{
             .filter_map(|item| item.ok())
             .map(|s| s.as_bytes().to_vec())
             .collect();
-        // println!("{:?}", byte_to_strings(&data));
-        tilt_north(&mut data);
-        // println!("{:?}", byte_to_strings(&data));
+        println!("{}\n\n", byte_to_strings(&data));
+        cycle(&mut data, 1000000000);
+        println!("{}", byte_to_strings(&data));
         let data_size = data.len();
         let ans: usize = data.iter()
             .enumerate()
